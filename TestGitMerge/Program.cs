5ca9754f043a6751b19303edf7eab6b089bf9d67
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-07-03 16:17:00 +0900

    Feat. cm를 입력받아서 inch로 바꾸는 기능 추가

--- TestGitMerge/Program.cs
+++ TestGitMerge/Program.cs
@@ -10,7 +10,17 @@ namespace TestGitMerge
     {
         static void Main(string[] args)
         {
-            Ruler ruler = new Ruler(10);
+            // {        2023. 07. 03.   Add user input / Beta
+            string userInput = string.Empty;
+            Console.WriteLine("This program convert Cm to Inch");
+            Console.WriteLine("Input Cm Value");
+            userInput = Console.ReadLine();
+
+            int cmInput = 0;
+            int.TryParse(userInput, out cmInput);
+
+            Ruler ruler = new Ruler(cmInput);
+            // }        2023. 07. 03.   Add user input / Beta
             ruler.Run();
         }
     }
